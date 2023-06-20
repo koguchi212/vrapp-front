1b0d643ab69b2127288e456ee08759f9b16fef32
koguchi212 <akira.kouguchi@gmail.com>
2023-06-20 17:50:59 +0900

    ブラウザ画面での音声認識に成功しました。

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import MyComponent from './MyComponent';
+import React from "react";
+import ReactSpeechRecognitionComponent from "./ReactSpeechRecognitionComponent";
 
 const App = () => {
   return (
-    <div className="App">
-      <MyComponent />
+    <div>
+      <h1>React Speech Recognition</h1>
+      <ReactSpeechRecognitionComponent />
     </div>
   );
 };
---
+++ src/ReactSpeechRecognitionComponent.js
@@ -0,0 +1,33 @@
+import React from "react";
+import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
+
+const ReactSpeechRecognitionComponent = () => {
+  const {
+    transcript,
+    listening,
+    resetTranscript,
+    browserSupportsSpeechRecognition,
+  } = useSpeechRecognition();
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>ブラウザが音声認識未対応です</span>;
+  }
+
+  return (
+    <div id="react-speech-recognition">
+      <p>入力: {listening ? "on" : "off"}</p>
+      <button type="button" onClick={() => SpeechRecognition.startListening()}>
+        入力開始
+      </button>
+      <button type="button" onClick={() => SpeechRecognition.stopListening()}>
+        Stop
+      </button>
+      <button type="button" onClick={() => resetTranscript()}>
+        リセット
+      </button>
+      <p>{transcript}</p>
+    </div>
+  );
+};
+
+export default ReactSpeechRecognitionComponent;
