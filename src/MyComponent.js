943e77679b2af7a80b3f00780068869bbd908bfc
koguchi212 <akira.kouguchi@gmail.com>
2023-06-23 12:27:41 +0900

    ボタンを押したことが分かるように調整しました

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Box, Button, Input, Heading, Text, Flex } from "@chakra-ui/react";
 import ReactSpeechRecognitionComponent from "./ReactSpeechRecognitionComponent";
 
@@ -6,6 +6,7 @@ const MyComponent = () => {
   const [prompt, setPrompt] = useState("");
   const [response, setResponse] = useState("");
   const [buttonColor, setButtonColor] = useState("green");
+  const [isLoading, setIsLoading] = useState(false);
 
   const handlePromptChange = (event) => {
     setPrompt(event.target.value);
@@ -17,6 +18,7 @@ const MyComponent = () => {
 
   const onClickHandler = async () => {
     try {
+      setIsLoading(true);
       const response = await fetch("http://127.0.0.1:5000/", {
         mode: "cors",
         method: "post",
@@ -28,34 +30,36 @@ const MyComponent = () => {
 
       const json = await response.json();
       setResponse(json["response"]);
-      setButtonColor("lightBlue"); // ボタンの色を変更
+      setIsLoading(false);
     } catch (error) {
       console.error(error);
+      setIsLoading(false);
     }
   };
 
-  const resetHandler = () => {
-    setPrompt("");
-    setResponse("");
-    setButtonColor("green"); // ボタンの色を元に戻す
-  };
+  useEffect(() => {
+    if (response) {
+      setButtonColor("green");
+    }
+  }, [response]);
 
   return (
     <Box>
       <ReactSpeechRecognitionComponent onResult={handleSpeechRecognitionResult} />
       <Flex mt={8}>
         <Input type="text" value={prompt} onChange={handlePromptChange} bgColor="white" borderColor="gray.400" mr={2} />
-        <Button onClick={onClickHandler} colorScheme={buttonColor}>
-          Submit
-        </Button>
-        <Button onClick={resetHandler} colorScheme="red" ml={2}>
-          Reset
+        <Button onClick={onClickHandler} colorScheme={buttonColor} isLoading={isLoading}>
+          {isLoading ? "送信中です" : "Submit"}
         </Button>
       </Flex>
       <Heading as="h2" size="md" mt={8}>
         chatgpt-apiを使って得た画像生成用プロンプト
       </Heading>
-      {response && <Text>{response}</Text>}
+      {isLoading ? (
+        <Text>送信中です</Text>
+      ) : (
+        response && <Text>{response}</Text>
+      )}
     </Box>
   );
 };
